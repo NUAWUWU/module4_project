8c2b895c417e4ff0e2832b6bba7447731a80ac54
Oleg <oleg.sharovski@mail.ru>
2023-07-28 17:15:21 +0500

    Template layer

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index, top_sellers
+from .views import index, top_sellers, advert_post, register, login, profile
 
 urlpatterns = [
     path('', index, name='main-page'),
     path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advert_post, name='advertisement-post'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('profile/', index, name='profile'),
 ]
