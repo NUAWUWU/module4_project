1791d7fd6597b885101e71581aeace27df0a5a32
Oleg <oleg.sharovski@mail.ru>
2023-08-11 20:24:55 +0500

    lesson 7 (ORM and Django Shell)

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField('заголовок', max_length = 128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits = 10, decimal_places = 2)
+    auction = models.BooleanField('торг', help_text='Отметьте если торг уместен')
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_date = models.DateTimeField(auto_now = True)
