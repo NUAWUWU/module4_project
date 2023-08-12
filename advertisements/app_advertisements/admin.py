a22b412990f3e20255053cb7943389cac23ae067
Oleg <oleg.sharovski@mail.ru>
2023-08-12 20:39:53 +0500

    bug fix and lesson 9 (Filling templates)

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -3,24 +3,15 @@ from .models import Advertisement
 from django.db.models.query import QuerySet
 
 class advertisements_Admin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_date', 'create_date', 'update_date']
+    list_display = ['id', 'user', 'title', 'description', 'price', 'auction', 'create_date', 'update_date', 'image']
     list_filter = ['created_date', 'price', 'auction']
     actions = ['make_auction_as_false', 'make_auction_as_true']
     fieldsets = (
         ('Общее', {
-            'fields' : (
-                'title', 'description'
-            ),
-                'classes' : ['collapse']
+            'fields' : ('title', 'description', 'user', 'image'),'classes' : ['collapse']}),
 
-        }),
         ('Финансы', {
-            'fields' : (
-                'price', 'auction'
-            ),
-                'classes' : ['collapse']
-        })
-    )
+            'fields' : ('price', 'auction'),'classes' : ['collapse']}))
 
     @admin.action(description='Убрать возможность торга')
     def make_auction_as_false(self, request, queryset:QuerySet):
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils import timezone
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Advertisement(models.Model):
     title = models.CharField('заголовок', max_length = 128)
@@ -12,6 +15,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField('торг', help_text='Отметьте если торг уместен')
     created_date = models.DateTimeField(auto_now_add = True)
     updated_date = models.DateTimeField(auto_now = True)
+    user = models.ForeignKey(User, verbose_name = 'пользователь', on_delete = models.CASCADE)
+    image = models.ImageField('изображение', upload_to='advertisements/')
 
     @admin.display(description='дата создания')
     def create_date(self):
@@ -21,12 +26,8 @@ class Advertisement(models.Model):
                 '<span style=color:green; font-weight: bolt>Сегодня в {}</span>',
                 created_time
             )
-        else:
-            return self.created_date.strftime('%D.%M.%Y at %H:%M:%S')
+        return self.created_date.strftime('%d.%m.%Y at %H:%M:%S')
 
-    def __str__(self):
-        return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
-    
     @admin.display(description='дата обновления')
     def update_date(self):
         if self.updated_date.date() == timezone.now().date():
@@ -35,8 +36,7 @@ class Advertisement(models.Model):
                 '<span style=color:green; font-weight: bolt>Сегодня в {}</span>',
                 updated_time
             )
-        else:
-            return self.updated_date.strftime('%D.%M.%Y at %H:%M:%S')
+        return self.updated_date.strftime('%d.%m.%Y at %H:%M:%S')
 
     def __str__(self):
         return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
